34ddc70ab056d047e88387c47aface3355e29c28
daiki taniguchi <t.daiki50@gmail.com>
2013-12-10 01:31:52 +0900

    add twilio logic

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -8,26 +8,29 @@ class CallsController < ApplicationController
     @call = Call.new
     @call.message =  params[:call][:message]
     @call.to =  params[:call][:to]
-    @call.save
+    if @call.save
 
-    @msg = params[:call][:message]
-    @sender = params[:call][:to]
+      @msg = params[:call][:message]
+      @sender = params[:call][:to]
 
-    #put your own credentials here
-    account_sid = 'ACb024b692d223ccb5a8988cba4073e226'
-    auth_token = '84d20c23c8d5496a9066668675362702'
+      #put your own credentials here
+      account_sid = 'ACb024b692d223ccb5a8988cba4073e226'
+      auth_token = '84d20c23c8d5496a9066668675362702'
 
-    # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
+      # set up a client to talk to the Twilio REST API
+      @client = Twilio::REST::Client.new account_sid, auth_token
 
-    @call = @client.account.calls.create(
-        :from => '+815031595958',   # From your Twilio number
-        :to => '+818013316810',     # To any number
-        # Fetch instructions from this URL when the call connects
-        :url => "http://twilio-call-client.herokuapp.com/calls/callback"
-    )
+      @call = @client.account.calls.create(
+          :from => '+815031595958',   # From your Twilio number
+          :to => '+818013316810',     # To any number
+          # Fetch instructions from this URL when the call connects
+          :url => "http://twilio-call-client.herokuapp.com/calls/callback"
+      )
 
-    render 'show'
+      render 'show'
+    else
+      render 'index'
+    end
   end
 
   def callback
--- app/models/call.rb
+++ app/models/call.rb
@@ -1,2 +1,4 @@
 class Call < ActiveRecord::Base
+  validates_presence_of :to
+  validates_presence_of :message
 end
