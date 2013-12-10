e96a352894c103952ea549b606214b55aa762333
daiki taniguchi <t.daiki50@gmail.com>
2013-12-10 09:59:50 +0900

    add twilio logic

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module TwilioClient
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #AssetPipelineによるコンパイル対象を増やす
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
